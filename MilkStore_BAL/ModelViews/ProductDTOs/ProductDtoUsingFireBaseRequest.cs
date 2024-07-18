bb955e3cf6e2e5ad355821d04504f1b2f2727272
Kindaka <nhatnguyen45678@gmail.com>
2024-07-19 00:31:20 +0700

    fixAddProductUsingFireBase

--- MilkStore_BAL/ModelViews/ProductDTOs/ProductDtoUsingFireBaseRequest.cs
+++ MilkStore_BAL/ModelViews/ProductDTOs/ProductDtoUsingFireBaseRequest.cs
@@ -13,7 +13,7 @@ namespace MilkStore_BAL.ModelViews.ProductDTOs
         public string ProductName { get; set; } = null!;
         public string ProductInfor { get; set; } = null!;
         public double ProductPrice { get; set; }
-        public int ProductQuatity { get; set; }
+        public int ProductQuantity { get; set; }
         public bool ProductStatus { get; set; }
 
         public List<IFormFile>? Images { get; set; }
