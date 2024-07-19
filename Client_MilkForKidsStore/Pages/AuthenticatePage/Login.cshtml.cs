21b431ac761fc0b2091683a4d533f52f20ed50bf
Nguyen Dang Khanh <khanndse161988@fpt.edu.vn>
2024-07-19 21:40:28 +0700

    login, logout razor page

---
+++ Client_MilkForKidsStore/Pages/AuthenticatePage/Login.cshtml.cs
@@ -0,0 +1,95 @@
+using Client_MilkForKidsStore.Pages;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using MilkStore_BAL.ModelViews.AccountDTOs;
+using Newtonsoft.Json;
+using System.ComponentModel.DataAnnotations;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using System.Text.Json.Nodes;
+
+namespace Client_MilkForKidsStore.AuthenticatePage
+{
+    public class LoginModel : PageModel
+    {
+        [BindProperty]
+        public UserAuthenticatingDtoRequest InfoView { get; set; }
+
+        private readonly HttpClient _httpClient;
+        private readonly IConfiguration _config;
+        private readonly ILogger<IndexModel> _logger;
+
+        public LoginModel(HttpClient httpClient, IConfiguration config, ILogger<IndexModel> logger)
+        {
+            _httpClient = httpClient;
+            _config = config;
+            _logger = logger;
+        }
+        public IActionResult OnGet()
+        {
+            return Page();
+        }
+        public async Task<IActionResult> OnPostAsync()
+        {
+
+            //var loginData = new { Email, Password };
+            var content = new StringContent(JsonConvert.SerializeObject(InfoView), Encoding.UTF8, "application/json");
+
+            var response = await _httpClient.PostAsync("https://localhost:7223/auth", content);
+
+            if (response.IsSuccessStatusCode)
+            {
+                var accessToken = JsonNode.Parse(await response.Content.ReadAsStringAsync())["accessToken"].ToString();
+
+                var handler = new JwtSecurityTokenHandler();
+                var jsonToken = handler.ReadToken(accessToken);
+                if (jsonToken is JwtSecurityToken tokenS)
+                {
+                    var roleClaim = tokenS.Claims.FirstOrDefault(x => x.Type == "RoleId");
+                    if (roleClaim != null)
+                    {
+                        var role = int.Parse(roleClaim.Value);
+
+                        
+
+                        if (role == 3)
+                        {
+                            Response.Cookies.Append("jsonToken", accessToken, new CookieOptions
+                            {
+                                HttpOnly = true,
+                                Secure = true,
+                                Expires = DateTimeOffset.UtcNow.AddHours(1)
+                            });
+                            return RedirectToPage("/Index");
+                        }
+
+                    }
+                    else
+                    {
+                        _logger.LogError("Role claim not found in the token.");
+                        ViewData["ErrorMessage"] = "Role claim not found in the token.";
+                    }
+                }
+            }
+
+            ModelState.AddModelError(string.Empty, "Invalid login attempt.");
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostLogout()
+        {
+            
+            Response.Cookies.Delete("jsonToken", new CookieOptions
+            {
+                HttpOnly = true,
+                Secure = true,
+                Path = "/"
+            });
+            return RedirectToPage("/Index");
+        }
+
+    }
+}
--- Client_MilkForKidsStore/Program.cs
+++ Client_MilkForKidsStore/Program.cs
@@ -1,10 +1,19 @@
+using Microsoft.AspNetCore.Authentication.Cookies;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddCors();
-builder.Services.AddSession();
 builder.Services.AddHttpClient();
+builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+        .AddCookie(options =>
+        {
+            options.LoginPath = "/AuthenticatePage/Login";
+        });
+builder.Services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
 
 var app = builder.Build();
 
@@ -17,14 +26,32 @@ if (!app.Environment.IsDevelopment())
 }
 
 app.UseCors();
-app.UseSession();
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
+app.Use(async (context, next) =>
+{
+    var token = context.Request.Cookies["jsonToken"];
+    if (!string.IsNullOrEmpty(token))
+    {
+        var handler = new JwtSecurityTokenHandler();
+        var jwtToken = handler.ReadToken(token) as JwtSecurityToken;
+        if (jwtToken != null)
+        {
+            var claims = jwtToken.Claims.Select(c => new Claim(c.Type, c.Value)).ToList();
+            var identity = new ClaimsIdentity(claims, "jwt");
+            var principal = new ClaimsPrincipal(identity);
+            context.User = principal;
+        }
+    }
+
+    await next();
+});
 app.MapRazorPages();
 
 app.Run();
